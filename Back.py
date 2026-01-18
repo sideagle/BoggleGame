c971831d9107f70210926e33e322cfa759aba90f
Priyanshu Lal <115842925+Hades916035@users.noreply.github.com>
2026-01-17 20:11:29 -0600

    Initial Commit

---
+++ Back.py
@@ -0,0 +1,16 @@
+from PyDictionary import PyDictionary
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+dictionary = PyDictionary()
+
+@app.route('/')
+def index():
+    """Connect"""
+    return render_template('.html')
+
+class InitGame:
+    make
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
